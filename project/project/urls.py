3f7b7db6cd8ddd538dc59539d3970b34f5c4e06d
AhmedZeins <AhmedZein8@gmail.com>
2023-01-22 18:21:50 +0200

    adding Register feature

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('donation.urls'))
+    path('',include('donation.urls')),
+    path('', include('register.urls')),
 ]
--- project/register/views.py
+++ project/register/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+
 
 # Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+    
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save
+            user = form.cleaned_data.get('username')
+            messages.success (request, 'Account was created for' + user)
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
