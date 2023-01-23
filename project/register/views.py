7dba74eb7f25c82d4cae2eea6178632658e94743
“ziad <zyaddarwish2000@gmail.com>
2023-01-23 09:24:34 +0200

    adding login

--- project/register/views.py
+++ project/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 
@@ -13,12 +14,19 @@ def registerPage(request):
         if form.is_valid():
             form.save
             user = form.cleaned_data.get('username')
-            messages.success (request, 'Account was created for' + user)
+            messages.success (request, 'Account was created for ' + user)
             return redirect('login')
     context = {'form':form}
     return render(request, 'register.html', context)
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:    
+            login(request, user)
+            return redirect('show')
     context = {}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'login.html', context)
